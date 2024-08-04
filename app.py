d1ea53c794ff563b9a01dc99a04b8e39ad39b5fc
Heleno Sango <122051145+helenosango2005@users.noreply.github.com>
2024-08-05 00:37:08 +0100

    Adicc

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-#importação
+# Importação
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
@@ -6,7 +6,17 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
 
 db = SQLAlchemy(app)
-#Definir uma rota raiz (Pagina inicial) e a função que será executada ao requisitar
+
+# Modelagem
+# Produto (id, name, price, description)
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.Text, nullable=True)
+
+
+# Definir uma rota raiz (Pagina inicial) e a função que será executada ao requisitar
 @app.route('/')
 def hello_world():
     return 'Hello World'
