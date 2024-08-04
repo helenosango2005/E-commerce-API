99ecf96a476090ec37431f8a5be059f9f13e890d
Heleno Sango <helenosango16@gmail.com>
2024-08-05 00:01:54 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,15 @@
+#importação
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
+
+db = SQLAlchemy(app)
+#Definir uma rota raiz (Pagina inicial) e a função que será executada ao requisitar
+@app.route('/')
+def hello_world():
+    return 'Hello World'
+
+if __name__ == "__main__":
+    app.run(debug=True)
